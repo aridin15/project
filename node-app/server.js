96d7f12433b4ecb91eb53b1dac1aa67d844d926d
ariel.amar <arielamar1995@gmail.com>
2024-12-01 15:19:44 +0200

    limit flask connection to cluster only

--- node-app/server.js
+++ node-app/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const axios = require('axios')
 
 app.get('/api/node', (req, res) => {
   res.send('Hello, World from Node.js!');
 });
+
+app.post('/api/fetch',async (req, res)=>{
+  try {
+    const result = await axios.get('http://35.236.33.159/api/flask');
+    res.status(200).json({
+      data: result.data
+    })
+  } catch (error){
+    res.status(500).json({
+      error: error.message
+    })
+  }
+})
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
