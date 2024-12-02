09ba87f39e28592ad442f24bf1eaa3ca4695df50
ariel.amar <arielamar1995@gmail.com>
2024-12-02 13:33:03 +0200

    fix js env name

--- node-app/server.js
+++ node-app/server.js
@@ -1,30 +1,28 @@
 const express = require('express');
 const app = express();
-const axios = require('axios')
-const dotenv = require('dotenv')
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 app.get('/api/node', (req, res) => {
   res.send('Hello, World from Node.js!');
 });
 
-app.post('/api/fetch',async (req, res)=>{
-  console.log('indide api fetch')
-  console.log(`process env flask uri is: ${process.env.FLASK_URI}`)
+app.post('/api/fetch', async (req, res) => {
+  console.log('inside api fetch');
+  console.log(`process env flask uri is: ${process.env.FLASK_API_URL}`);
   try {
-    const result = await axios.get(process.env.FLASK_URI);
+    const result = await axios.get(process.env.FLASK_API_URL);
     res.status(200).json({
       data: result.data
-    })
-  } catch (error){
+    });
+  } catch (error) {
     res.status(500).json({
       error: error.message
-    })
+    });
   }
-})
-
-
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
